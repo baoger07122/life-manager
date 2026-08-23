c319e25ebfad2c4b50654cd62677644ab17b3395
hui-baopc <zhliu95@163.com>
2026-08-23 17:37:07 +0800

    Release v8.23.9 optional haptic feedback

--- update-check.js
+++ update-check.js
@@ -1,5 +1,5 @@
-const APP_VERSION = "v8.23.8";
-const APP_RELEASE = "个人管家 v8.23.8 - 新增食品保存按钮改为居中满宽样式";
+const APP_VERSION = "v8.23.9";
+const APP_RELEASE = "个人管家 v8.23.9 - 增加点击触感反馈";
 
 const compareVersions = (left, right) => {
   const parse = (value) => value.replace(/^v/i, "").split(".").map((part) => Number.parseInt(part, 10) || 0);

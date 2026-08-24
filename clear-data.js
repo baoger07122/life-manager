b45a9b25293ffb56eb690acae29d5028be2d8dfc
hui-baopc <zhliu95@163.com>
2026-08-24 19:16:33 +0800

    Refine long-hop navigation transition

--- clear-data.js
+++ clear-data.js
@@ -18,7 +18,7 @@ const clearLocalData = () => {
 
   window.localStorage.setItem("home-os-v1", JSON.stringify(EMPTY_HOME_STATE));
   const hash = window.location.hash || "#home";
-  window.location.replace(`${window.location.pathname}?v=8.24.5&refresh=${Date.now()}${hash}`);
+  window.location.replace(`${window.location.pathname}?v=8.24.6&refresh=${Date.now()}${hash}`);
 };
 
 const ensureClearDataRow = () => {
